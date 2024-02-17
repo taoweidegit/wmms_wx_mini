8af18af045c41afd40435d2942a6a3d10134ca69
Your Name <you@example.com>
2024-02-17 23:03:39 +0800

    commit
    commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,7 @@
 Page({
   data: {
     employee: '',
-    token: ''
+    is_bind: false
   },
   onLoad(){
     // 页面加载时，自动获取微信Id，若已完成绑定，页面自动跳转
@@ -13,7 +13,23 @@ Page({
     // 获取微信Id
     wx.login({
       success: (result) => {
-        this.data.token = result.code
+        wx.request({
+          method: 'GET',
+          url: 'http://116.198.36.208:5000/wx/login',
+          data: {
+            'code': result.code,
+            'employee': this.data.employee
+          },
+          success: (result) => {
+            console.log(result.code)
+            if (result.code == 200)
+              this.data.is_bind=true;
+            else if (result.code == 500)
+              wx.showToast({
+                title: '请求错误',
+              })
+          }
+        });
       },
       fail: (err) => {
         console.log(err.errMsg)
@@ -23,12 +39,11 @@ Page({
   },
   login(e){
     // 第一次将工号与微信进行绑定
-    if (this.data.token == '')
-      this.wx_login();
-    console.log(this.data.employee + ' ' + this.data.token)
-    wx.showToast({
-      title: '绑定成功',
-    })
+    this.wx_login();
+    if (this.data.is_bind)
+      wx.showToast({
+        title: '绑定成功',
+      })
   },
   bindKeyInput(e){
     this.setData({
